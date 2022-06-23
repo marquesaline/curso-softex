b7aa1026ca1995655f7c927d6283a61210ec8944
marquesaline <alinem_oliveira@yahoo.com>
2022-06-23 20:50:48 -0300

    feat: atividade15

---
+++ modulo3/atividade15.py
@@ -0,0 +1,20 @@
+# Faça um código que execute a contagem regressiva de uma bomba, informando o número de segundos para explodir. Ele deverá mostrar a mensagem “iniciando contagem regressiva”, os segundos passados e, no final, a mensagem “BUM!”.
+
+# Não é necessário, mas, caso deseje tornar o sistema mais realista para que o tempo realmente passe em segundos, você pode usar a função time.sleep() que existe na Python (acesse o link em anexo). No entanto, é preciso adicionar uma biblioteca antes de executá-la. 
+
+ 
+import time
+
+i = 10
+
+
+print("Começando a contagem regressiva")
+while (i != 0):    
+    print(i)
+    time.sleep(1)
+    i = i - 1
+
+print("BUMM")
+
+
+
